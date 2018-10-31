66009b12d5dff78f8a59dac15cf10933d0cea858
Elizabeth Creech <elizabeth@timbergrove.com>
2018-10-30 21:32:09 -0500

    working to the winning message - need to add reset and pebbles graphic

--- nim/main.js
+++ nim/main.js
@@ -5,16 +5,20 @@
 var content = document.getElementById('content')
 var containerEl = document.getElementById('container')
 
-const gameState = {
-    playerTurn: 1,
+// gameState is the var that was called Nim in the template code
+const initialGameState = {
+    playerTurn: 1, 
     NumOfPebbles: 16,
     winner: null
 }
+
+let gameState = deepCopy(initialGameState)
 
 let theGame = deepCopy(gameState)
 
 function resetGame () {
     theGame = deepCopy(initialGameState)
+    console.log('Resetting the Game!')
     renderGame()
   }
 
@@ -25,22 +29,26 @@ function onTake () {
     var takePebbles = document.getElementById('takeInput')
     gameState.NumOfPebbles = gameState.NumOfPebbles - takePebbles.value
     console.log('You took ' + takePebbles.value + ' Pebbles')   
-    checkWinnerState ()
+    checkWinnerState()  
 }
 
 function checkWinnerState () {
-  if (gameState.NumOfPebbles <= 0) {
-    gameState.winner = gameState.playerTurn
-    buildWinnerMsg()
+    console.log('Winner set to ' + gameState.winner)
+  if (gameState.NumOfPebbles === 1) {
+      console.log('Made to only ONE pebble')
+      gameState.winner = gameState.playerTurn
+      buildWinnerMsg()
+      renderGame()
   } else {
     if (gameState.playerTurn === 1) {
         gameState.playerTurn = 2
-    }
-    else
-    gameState.playerTurn = 1
-  }
-  console.log('it is Player ' + gameState.playerTurn + 's Turn!')
-    buildPebblesLeftMsg()
+    } 
+    else {
+        gameState.playerTurn = 1
+    } }
+    init()
+  
+    console.log('it is Player ' + gameState.playerTurn + 's Turn!') 
 }
 
 // -----------------------------------------------------------------------------
@@ -50,7 +58,6 @@ function checkWinnerState () {
 function buildPebblesLeftMsg () {
     console.log('There are ' + gameState.NumOfPebbles + ' Pebbles Left')
     return '<h4>There are ' + gameState.NumOfPebbles + ' pebbles left</h4>'
-    buildPlayerTurnMsg()
 }
 
 // function buildPebbleGraphic (NumOfPebbles) {
@@ -63,62 +70,58 @@ function buildPebblesLeftMsg () {
 // }
 
 function buildPlayerTurnMsg () {
-    return '<h4>It is player' + gameState.playerTurn + 's turn! How many pebbles will you take?</h4>'
+    return '<h4>It is player ' + gameState.playerTurn + 's turn! How many pebbles will you take?</h4>'
 }
 
 function buildWinnerMsg () {
-    console.log('Player ' + gameState.winner + ' WINS! There are ' + gameState.NumOfPebbles + ' Pebbles Left')
+    
+    return '<h4>Player '+ gameState.winner + ' WINS! Click the Take button to Reset the game.</h4>'
 }
 
 function buildQtySelector () {
     // Check remaining quauntity; if < 3
     if (gameState.NumOfPebbles >= 3) {
+        // build selector with 3 options
         return '<div><select id="takeInput"><option value="1">1</option><option value="2">2</option><option value="3">3</option></select>'
-    // build selector with 3 options
-    console.log('There are 3 or more pebbles left')
     } else if (gameState.NumOfPebbles === 2) {
-    // build selector with 2 options
-    console.log('There only 2 pebbles left')
+        // build selector with 2 options
+        console.log('Hey! Only 2 Options')
+        return '<div><select id="takeInput"><option value="1">1</option><option value="2">2</option></select>'
     }  else {
-    // build selector with only 1 option
-    console.log('Only 1 pebble left')
+        // build selector with only 1 option
+        console.log('Hey! Only 1 Option')
+        return '<div><select id="takeInput"><option value="1">1</option></select>'
     }  
 }
 
 function buildTakebutton () {
     return  '<button id="takeButton">Take</button>'
 }
-//"Replace this with your own abstraction of Nim"
-var nim = {
-    //'<h1>NIM>/h1>' +
-    //'<div>There are ' + gameState.NumOfPebbles + ' pebbles left</div>'
-    
-}
-
 
 
 function buildGame (game) {
     console.log('got to buildGame function')
     let html = '<div class="container d-flex flex-column justify-content-start align-items-center">'
+    if (gameState.winner != null) {
+        console.log('Player ' + gameState.winner + ' WINS! There are ' + gameState.NumOfPebbles + ' Pebbles Left')
+        html += buildWinnerMsg(gameState)
+    } else {
         html += buildPebblesLeftMsg(gameState)
         html += buildPlayerTurnMsg(gameState)
         html += buildQtySelector(gameState)
         html += buildTakebutton(gameState)
-     return html
+    }
+    return html
  }
- //content.innerHTML = renderGame(nim);
+
 
 // -----------------------------------------------------------------------------
 // Events
 // -----------------------------------------------------------------------------
 function addEvents () {
-  console.info('Adding DOM events now')
-  //content.addEventListener('click', clickContainer)
+  //console.info('Adding DOM events now')
   buttonEl.addEventListener('click', onTake)
 }
-
-
-
 
 // -----------------------------------------------------------------------------
 // Rendering
@@ -130,43 +133,44 @@ let renderCount = 0
 function renderGame() {
 
     // Change this render function to use the "game" parameter//
-    //renderCount++
+    renderCount++
     console.info('Rendering game now! Render #' + renderCount)
-    content.innerHTML = buildGame(theGame)
+    console.log('Got the the renderGame function')
+    return content.innerHTML = buildGame(gameState)
 
-console.log('Got the the renderGame function')
-    return `
-        <div class="container d-flex flex-column justify-content-start align-items-center">
-            <h4>There are 16 pebbles left</h4>
-            <div class="w-50 text-center pebble-container">
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-            </div>
-            <h4 class="mt-5">It's player 1's turn! How many pebbles will you take?</h4>
-            <div>
-                <select id="takeInput">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-                <button id="takeButton">Take</button>
-            </div>
-        </div>
-    `
+
+    // return `
+    //     <div class="container d-flex flex-column justify-content-start align-items-center">
+    //         <h4>There are 16 pebbles left</h4>
+    //         <div class="w-50 text-center pebble-container">
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //             <div class="pebble"></div>
+    //         </div>
+    //         <h4 class="mt-5">It's player 1's turn! How many pebbles will you take?</h4>
+    //         <div>
+    //             <select id="takeInput">
+    //                 <option value="1">1</option>
+    //                 <option value="2">2</option>
+    //                 <option value="3">3</option>
+    //             </select>
+    //             <button id="takeButton">Take</button>
+    //         </div>
+    //     </div>
+    // `
 }
 
 // -----------------------------------------------------------------------------
@@ -187,17 +191,20 @@ console.log('Got the the renderGame function')
 
 function init () {
   console.info('Initializing Nim now!')
+  renderGame()
   content = document.getElementById('content')
   selectEl = document.getElementById('takeInput')
   buttonEl = document.getElementById('takeButton')
-  renderGame()
   addEvents()
   console.info('passed call to renderGame in Init function')
 }
+
+// -----------------------------------------------------------------------------
+// Utility
+// -----------------------------------------------------------------------------
 
 function deepCopy (x) {
     return JSON.parse(JSON.stringify(x))
   }
 
-// content.innerHTML = renderGame();
 document.addEventListener('DOMContentLoaded', init)
